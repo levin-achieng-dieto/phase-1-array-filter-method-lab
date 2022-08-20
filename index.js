872b01d3ee2fecb3909f9ebad75efecef40c7e8d
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-08-20 12:27:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(names, drivers){
+  return names.filter(
+    (matchingNames) => matchingNames.toLowerCase() === drivers.toLowerCase()
+  )
+}
+
+function fuzzyMatch(name, firstLatter){
+  return name.filter(
+    (matchingNames) = matchingNames.indexOf(firstLatter.toLowerCase()) === "sa"
+  )
+}
+
+
+function matchName (person, propertiesByName){
+  return person.filter(
+    details => details.name === propertiesByName
+  )
+}
+
